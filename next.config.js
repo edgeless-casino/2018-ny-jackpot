c7698f1b51c71efd54faa0c07dcd9d48038c8869
Rytis Grincevicius <rytis.grincevicius@gmail.com>
2019-01-02 13:25:14 +0200

    Asset build for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
-  assetPrefix: isProd ? '/2018-ny-jackpot/' : '',
+  assetPrefix: isProd ? 'https://edgeless-casino.github.io/2018-ny-jackpot/' : '',
   exportPathMap: function () {
     return {
       '/': { page: '/' }
